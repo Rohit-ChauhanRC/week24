bc6efa6b95e25372397a3dec792190e389a465f8
ROHIT CHAUHAN <rc672800@gmail.com>
2023-03-02 16:44:01 +0530

    use promise

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 import fs from 'fs';
 
-fs.readFile('async.txt', function (err, data) { 
-    if (err) {
-        console.error(err);
-    } else {
-        console.log(data.toString());
-    }
-});
+// fs.readFile('async.txt', function (err, data) {
+//     if (err) {
+//         console.error(err);
+//     } else {
+//         console.log(data.toString());
+//     }
+// });
+
+const getFile = fileName => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(fileName, (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data.toString());
+            }
+        })
+    })
+}
+
+getFile("async.txt")
+  .then((data) => console.log(data))
+  .catch((err) => console.error(err));
