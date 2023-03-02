34e5cd10b73271fb5c41a98242fc05df3281f55c
ROHIT CHAUHAN <rc672800@gmail.com>
2023-03-02 16:52:55 +0530

    async & await use

--- index.js
+++ index.js
@@ -8,18 +8,30 @@ import fs from 'fs';
 //     }
 // });
 
-const getFile = fileName => {
-    return new Promise((resolve, reject) => {
-        fs.readFile(fileName, (err, data) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(data.toString());
-            }
-        })
-    })
+// const getFile = fileName => {
+//     return new Promise((resolve, reject) => {
+//         fs.readFile(fileName, (err, data) => {
+//             if (err) {
+//                 reject(err);
+//             } else {
+//                 resolve(data.toString());
+//             }
+//         })
+//     })
+// }
+
+// getFile("async.txt")
+//   .then((data) => console.log(data))
+//   .catch((err) => console.error(err));
+
+
+async function getFile(fileName
+) {
+    let data = fs.promises.readFile(fileName);
+    // return (await data).toString();
+    return  (await data).toString();
 }
 
-getFile("async.txt")
-  .then((data) => console.log(data))
-  .catch((err) => console.error(err));
+// console.log(await getFile("async.txt"));
+let dtaGet = await getFile('async.txt');
+console.log(dtaGet);
